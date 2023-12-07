799d4b65cf5111f6da2808bc8819b6ea0e757adc
SergioAndrade22 <sergio.david2201@gmail.com>
2023-12-06 21:23:35 -0300

    Removes unnecesary services & restructures folders

--- src/app/services/converter.ts
+++ src/app/services/converter.ts
@@ -3,11 +3,12 @@ import {
     QueryDocumentSnapshot,
     SnapshotOptions,
 } from '@angular/fire/firestore'
-import { Position } from '../../types'
+import { Position } from '../types'
 
 export const converter = {
     toFirestore(position: Position): DocumentData {
       return { 
+        code: position.code,
         latitude: position.latitude,
         longitude: position.longitude,
         timestamp: position.timestamp,
@@ -19,6 +20,7 @@ export const converter = {
     ): Position {
       const data = snapshot.data(options)
       return { 
+        code: data['code'],
         latitude: data['latitude'],
         longitude: data['longitude'],
         timestamp: data['timestamp'],
