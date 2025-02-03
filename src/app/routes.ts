b7225cbdb3e8ddf1ccb241ac684e5cc2ddf6b0cb
sergio-andrade <sergio.andrade@mheducation.com>
2025-02-03 12:09:03 -0300

    Fixes pathing for build

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router'
 import { TablesComponent } from './tables/tables.component'
 
 export const ROUTES: Routes = [
-    { path: "", pathMatch: "full", redirectTo: 'tablas' },
+    { path: "", redirectTo: 'tablas' },
     { path: "tablas", pathMatch: "full", component: TablesComponent  },
     { path: "**", component: TablesComponent },
 ]
