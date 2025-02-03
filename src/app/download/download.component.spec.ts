f0b04eb86471985165608c6c9ebb84b529fa7c68
sergio-andrade <sergio.andrade@mheducation.com>
2025-02-03 11:51:57 -0300

    Fixing linting for spec tests

--- src/app/download/download.component.spec.ts
+++ src/app/download/download.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { DownloadComponent } from './download.component';
+import { DownloadComponent } from './download.component'
 
 describe('DownloadComponent', () => {
-  let component: DownloadComponent;
-  let fixture: ComponentFixture<DownloadComponent>;
+  let component: DownloadComponent
+  let fixture: ComponentFixture<DownloadComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DownloadComponent]
-    });
-    fixture = TestBed.createComponent(DownloadComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [DownloadComponent],
+    })
+    fixture = TestBed.createComponent(DownloadComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
